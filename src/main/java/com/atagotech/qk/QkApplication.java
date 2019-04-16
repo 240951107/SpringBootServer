a61763e3568213cf6f24076faf24784c165b9220
tang <240951107@qq.com>
2019-04-17 00:53:48 +0800

    init

---
+++ src/main/java/com/atagotech/qk/QkApplication.java
@@ -0,0 +1,13 @@
+package com.atagotech.qk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QkApplication.class, args);
+    }
+
+}
